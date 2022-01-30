eb8902d25d9971058d1c122d7d96e42ebc5fe34b
Emanuele Ricci <stermi@gmail.com>
2022-01-30 20:16:41 +0100

    updated solidity compiler version used to 0.8.10

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -21,7 +21,7 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 const config: HardhatUserConfig = {
-  solidity: '0.8.4',
+  solidity: '0.8.10',
   networks: {
     rinkeby: {
       url: process.env.RENKEBY_URL || '',
